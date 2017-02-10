d8d47e54d9c6a33f86d9778c0dacd3e13f6decf1
unknownpers <unknownpersone412@gmail.com>
2017-02-10 00:07:01 +0000

    Basic Stuff 0.1

---
+++ main.h
@@ -0,0 +1,8 @@
+#define MODULE_NAME         "Djikstra Module"
+#define MODULE_AUTHOR       "Unknown"
+#define MODULE_VERSION      1.0
+
+#include <string.h>
+#include "CFunctions.h"
+#include "Common.h"
+#include "include\ILuaModuleManager.h"
